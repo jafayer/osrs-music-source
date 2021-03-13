6f03cefb58f5fb1fc762c6828776abc28be3b44a
Josh <github@fayer.me>
2021-03-13 16:06:06 -0500

    added id field to object

--- src/editdata.js
+++ src/editdata.js
@@ -7,11 +7,17 @@ fsPromises.readFile('./data.json')
 .then(res => JSON.parse(res))
 .then(json => {
     /* insert code here */
-    let queue = ["Newbie Melody", "Yesteryear", "Autumn Voyage", "Adventure", "Harmony", "Fishing", "Garden", "Shine", "Home Sweet Home", "Background", "Dangerous", "Sea Shanty2", "Fanfare", "Baroque", "Barbarianism", "Work Work Work", "Camelot", "All's Fairy in Love and War", "Spirit", "Crystal Sword", "Doorways", "Nightfall", "Lightwalk", "Still Night", "Sea Shanty", "Pinball Wizard", "Wrath and Ruin", "Scape Main"];
-    let queueSongs = queue.map(song => json.songs.find(j => j.title === song));
-    json.queue = queueSongs;
-    json.queue.forEach(song => console.log(song.filename));
-    // fsPromises.writeFile('./newdata.json',JSON.stringify(json), () => {
-    //     console.log("DONE!");
-    // });
+    let songs = json.songs;
+    for(let song in songs) {
+        songs[song].id = song;
+    }
+
+    json.songs = songs;
+    console.log(json);
+    console.log(json.version);
+    console.log(json.songs.length);
+    console.log(json.queue.length);
+    fsPromises.writeFile('./newdata.json',JSON.stringify(json), () => {
+        console.log("DONE!");
+    });
 });
